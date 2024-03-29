5d0c1e63b34f31009f5eae4d56c6f9ca2a48c651
yw.hao <yw.hao@trip.com>
2024-03-29 19:24:14 +0800

    feat: add example

--- rnDemo/src/View/ComponentApiTest.tsx
+++ rnDemo/src/View/ComponentApiTest.tsx
@@ -4,11 +4,13 @@ import {View, Text, Button} from 'react-native';
 const App: React.FC<any> = () => {
   return (
     <View style={{flex: 1}}>
-      <View style={{width: 7000, height: 20}}>
+      <View style={{width: 70, height: 20}}>
+        {/* @description 只有设置了字体大小 才会变大或者变小 */}
         <Text adjustsFontSizeToFit={true} style={{fontSize: 40}}>
           测试adjustsFontSizeToFit
         </Text>
       </View>
+      <Text style={{fontSize: 40}}>测试adjustsFontSizeToFit</Text>
     </View>
   );
 };
