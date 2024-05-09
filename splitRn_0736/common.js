469c8aceec3883afe38c9ad9620b3336fe6b3173
Hao-yiwen <1552975589@qq.com>
2024-05-10 01:09:17 +0800

    feat: add split code

---
+++ splitRn_0736/common.js
@@ -0,0 +1,13 @@
+import 'react';
+import 'react-native';
+
+if (!__DEV__) {
+  console = {
+    info: () => {},
+    log: () => {},
+    warn: () => {},
+    error: () => {},
+    time: () => {},
+    timeEnd: () => {},
+  };
+}
