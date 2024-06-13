2b56b18d4e2ed5bea7644f02bf2633a37bf7fc11
Hao-yiwen <1552975589@qq.com>
2024-06-14 02:33:13 +0800

    feat: add react-native-webview

--- rnDemo0742/App.tsx
+++ rnDemo0742/App.tsx
@@ -1,49 +1,16 @@
 import React from 'react';
 import {Dimensions, ScrollView, Text, View} from 'react-native';
+import WebView from 'react-native-webview';
 
-const App = () => {
+const App = props => {
+  console.log('App props:', props);
   return (
     <View style={{flex: 1, flexDirection: 'row'}}>
-      <ScrollView
-        style={{
-          width: 300,
-          height: Dimensions.get('screen').height,
-        }}>
-        {Array.from({length: 50}).map((_, index) => (
-          <View
-            key={index}
-            style={{
-              height: 20,
-              width: '100%',
-              marginTop: 20,
-              backgroundColor: 'orange',
-            }}>
-            <Text>
-              {index + 1}. Lorem ipsum dolor sit amet, consectetur adipiscing
-            </Text>
-          </View>
-        ))}
-      </ScrollView>
-      <ScrollView
-        style={{
-          width: 300,
-          height: Dimensions.get('screen').height,
-        }}>
-        {Array.from({length: 500}).map((_, index) => (
-          <View
-            key={index}
-            style={{
-              height: 20,
-              width: '100%',
-              marginTop: 20,
-              backgroundColor: 'green',
-            }}>
-            <Text>
-              {index + 1}. Lorem ipsum dolor sit amet, consectetur adipiscing
-            </Text>
-          </View>
-        ))}
-      </ScrollView>
+      <WebView
+        style={{flex: 1}}
+        source={{uri: 'https://www.google.com'}}
+        scalesPageToFit={true}
+      />
     </View>
   );
 };
